a39795dba14a9550dec12339905f7f14658c0d78
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-11-01 04:50:36 -0400

    unhandled promise rejection error

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-const express = require("express");
-const app = express();
+require("dotenv").config()
+const multer = require("multer")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const File = require("./models/File")
 
-app.set("view engine", "ejs");
+const express = require("express")
+const app = express()
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req,res) => {
+const upload = multer({ dest: "uploads" })
+
+mongoose.connect(process.env.DATABASE_URL)
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
   res.render("index")
-});
+})
 
-app.listen(3002);
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  }
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10)
+  }
+
+  const file = await File.create(fileData)
+
+})
+
+app.listen(process.env.PORT);
